2b7f213e5ee36d2b11655237248f1f87c793e39d
Dumitrica Dan <dumitricadan@yahoo.com>
2018-09-19 19:12:12 +0300

    Updated gitignore file

--- src/ro/jademy/millionaire/Question.java
+++ src/ro/jademy/millionaire/Question.java
@@ -4,9 +4,29 @@ import java.util.List;
 
 public class Question {
     public static final int MAX_LEVEL = 15;
-    String question;
-    int difficultyLevel;
-    List<Answer> answerList = new ArrayList<>();
+    private String question;
+    private int difficultyLevel;
+    private List<Answer> answerList = new ArrayList<>();
+
+    public List<Answer> getAnswerList () {
+        return this.answerList;
+    }
+    public void setAnswerList(java.util.List<Answer> answerList) {
+        this.answerList = answerList;
+    }
+    public int getDifficultyLevel() {
+        return this.difficultyLevel;
+    }
+    public void setDifficultyLevel(int difficultyLevel) {
+        this.difficultyLevel = difficultyLevel;
+    }
+    public String getQuestion() {
+        return this.question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
 
     public Answer getCorrectAnswer () {
         for (Answer answer: answerList) {
